825445d37bb22e6f504a6e4c630edafc3440c209
Davron Abdukhakimov <davron2806@gmail.com>
2022-05-07 13:05:47 +0500

    added react file and started frontend part

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const { chats } = require('./data/data');
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("API is RUnnig");
+    res.send("API is RUnnig successfully");
 });
 
-app.get()
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
 
-app.listen(5500, console.log("Server started on PORT 5500"));
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5500
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
